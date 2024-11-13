a0e12717cf8c9db42ddccbb5a52544fbfea7e7e9
Apshirazi <ashirazi2@huskers.unl.edu>
2024-11-13 11:40:47 -0500

    parallel_mpc

--- scripts/IDC_Lab.py
+++ scripts/IDC_Lab.py
@@ -684,7 +684,6 @@ def parallel_movement(
         robots[idx].plan(tcp_name="tool1",
                          target_pose=target_pose,
                          target_orientation= target_orientation)
-        print("ROBOT_" + robots[idx]._ROS_JS_robot_indicator + "Path Waypoints:" + str(len(robots[idx]._computed_cmd_plan.position)))
 
     cmd_idx = 0
     while cmd_idx < len(robots[Rob_idx_list[0]]._computed_cmd_plan.position):
@@ -704,6 +703,53 @@ def parallel_movement(
                 test._my_world.step(render=False)
         
         cmd_idx += 1
+
+# Timer to check for updating the 
+cc_world_step_updater = 100
+
+def mpc_movement(
+    Rob_idx_list: List[int] = [0, 1],
+    Goal_List_Pose: List[np.ndarray] = [np.array([-0.49, -1.54, 0.44], dtype=float), np.array([-0.022, -1.85, 0.57], dtype=float)],
+    Goal_List_Orientation: List[np.ndarray] = [np.array([1, 0, 0, 0], dtype=float), np.array([1, 0, 0, 0], dtype=float)]
+):
+    while 1>0:
+        for idx in Rob_idx_list:
+            # Ensure each pose and orientation is in the correct format
+            target_pose = np.array(Goal_List_Pose[idx][:3], dtype=float).reshape(3)
+            target_orientation = np.array(Goal_List_Orientation[idx][:4], dtype=float).reshape(4)
+
+            # Call plan with the correctly shaped pose and orientation
+            robots[idx].plan(tcp_name="tool1",
+                            target_pose=target_pose,
+                            target_orientation= target_orientation)
+
+        Traj_Ending_Flag = False
+        cmd_idx = 0
+        Starting_Step = test._my_world.current_time_step_index
+        while cmd_idx < len(robots[Rob_idx_list[0]]._computed_cmd_plan.position):
+            test._my_world.step(render=True)
+            if(test._my_world.current_time_step_index - Starting_Step >= cc_world_step_updater):
+                break
+            
+            for idx in Rob_idx_list:
+                cmd_state = robots[idx]._computed_cmd_plan[cmd_idx]
+
+                # Apply articulation action
+                art_action = ArticulationAction(
+                    cmd_state.position.cpu().numpy(),
+                    cmd_state.velocity.cpu().numpy(),
+                    joint_indices=robots[idx]._computed_idx_list,
+                )
+                robots[idx]._articulation_controller.apply_action(art_action)
+                for _ in range(2):
+                    test._my_world.step(render=False)
+            
+            cmd_idx += 1
+            if cmd_idx == len(robots[Rob_idx_list[0]]._computed_cmd_plan.position):
+                Traj_Ending_Flag = True
+        if Traj_Ending_Flag == True:
+            print("Trajectory Done")
+            break
         
 
 def main():
@@ -774,7 +820,7 @@ def main():
         
         # robots[0].render_exec(renderInstance=True)
 
-        parallel_movement(Goal_List_Orientation=[np.array([quat[0], quat[1], quat[2], quat[3]]),
+        mpc_movement(Goal_List_Orientation=[np.array([quat[0], quat[1], quat[2], quat[3]]),
                                                  np.array([quat_test[0], quat_test[1], quat_test[2], quat_test[3]])])
 
 
