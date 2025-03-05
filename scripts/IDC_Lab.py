0f39cc829698772c0d069076d1cc4820a4ab03c3
apshirazi <ashirazi2@huskers.unl.edu>
2025-03-05 12:56:34 -0500

    L/U not Doable, BearLoading Done

--- scripts/IDC_Lab.py
+++ scripts/IDC_Lab.py
@@ -5128,19 +5128,43 @@ def L_U(el_name: str = None,
     # Moving Conveyor To End
     Smart_Conv.render_exec('Joint_1', SMART_CONV_RANGE_OF_MOTION_J1)
 
-    PASSING_ELEVATION: float = 0.85
-    # Passing To Rob 1
+    # PASSING_ELEVATION: float = 0.4
+    # # Passing To Rob 1
     
+    # Robot_2.plan(tcp_name= "tool0",
+    #                 target_pose= [2.3+(L/2)-SLOPED_TABLE_PICK_OFFSET_FROM_L_CORNER-(ROBOT_2_GRIPPER_LENGTH/2),
+    #                               -1,
+    #                               PASSING_ELEVATION+(H)],
+    #                 target_orientation= [0, ev, ev, 0],
+    #                 update_world_needed= True,
+    #                 removing_primitives=["world/obstacles", "Smart_Conveyor"],)
+    # Robot_2.render_exec(renderInstance= True,
+    #                         Show_Sphere= False)
+# Reached Pose: ['3.09', '1.03', '1.53'], Reached Orientation: [array([-0.49999565,  0.50000435,  0.49999565,  0.50000435], dtype=float32)]
+
     Robot_2.plan(tcp_name= "tool0",
-                    target_pose= [2.3+(L/2)-SLOPED_TABLE_PICK_OFFSET_FROM_L_CORNER-(ROBOT_2_GRIPPER_LENGTH/2),
-                                  -1,
-                                  PASSING_ELEVATION+(H)],
-                    target_orientation= [0, ev, ev, 0],
-                    update_world_needed= True)
+                    target_pose= [3.09,
+                                  1.03,
+                                  1.53],
+                    target_orientation= [-0.49999565,  0.50000435,  0.49999565,  0.50000435],
+                    update_world_needed= True,
+                    removing_primitives=["world/obstacles"],)
     Robot_2.render_exec(renderInstance= True,
                             Show_Sphere= False)
 
-    Robot_1.free_TCP_movement("tool1")
+# Reached Pose: ['1.16', '1.00', '1.74'], Reached Orientation: [array([-0.70090926,  0.71325046,  0.        ,  0.        ], dtype=float32)]
+    Robot_1.plan(tcp_name= "tool1",
+                    target_pose= [1.16, 1, 1.74],
+                    target_orientation= [-0.70090926,  0.71325046,  0.        ,  0.        ],
+                    update_world_needed= True,
+                    removing_primitives=["world/obstacles", "Smart_Conveyor"],)
+    Robot_1.render_exec(renderInstance= True,
+                            Show_Sphere= False)
+
+    # while True:
+    #     Robot_2.free_TCP_movement("tool0")
+    #     Robot_1.free_TCP_movement("tool1")
+
 ###########
 ####END####
 ###########
@@ -5224,15 +5248,15 @@ def main():
         # TPL("Wooden_Element_1", 0.02, SMART_MAT_TABLE_MAX_LENGTH/2, 0.06, SMART_MAT_TABLE_MAX_LENGTH, 0.04, STUD_HEIGHT)
 
         # # # Door
-        KING("Wooden_Element_6", 1.2592, 1.52, 0, 2.4384, 0.04, STUD_HEIGHT)
-        KING("Wooden_Element_7", 1.2592, 2.52, 0, 2.4384, 0.04, STUD_HEIGHT)
-        LJCK("Wooden_Element_12", 1.4784, 1.56, 0, 2, 0.04, STUD_HEIGHT)
-        RJCK("Wooden_Element_11", 1.4784, 2.48, 0, 2, 0.04, STUD_HEIGHT)
+        # KING("Wooden_Element_6", 1.2592, 1.52, 0, 2.4384, 0.04, STUD_HEIGHT)
+        # KING("Wooden_Element_7", 1.2592, 2.52, 0, 2.4384, 0.04, STUD_HEIGHT)
+        # LJCK("Wooden_Element_12", 1.4784, 1.56, 0, 2, 0.04, STUD_HEIGHT)
+        # RJCK("Wooden_Element_11", 1.4784, 2.48, 0, 2, 0.04, STUD_HEIGHT)
         # TSP("Small_Stud_1", 0.4584, 2.02, 0, 0.96, 0.04, STUD_HEIGHT)
         # TCP("Small_Stud_5", 0.2392, 2.02, 0, 0.3984, 0.04, STUD_HEIGHT)
 
-        BL("Wooden_Element_13", 0.4784-(RAW_HEADER_DIMENSIONS[2]/2), 2.02, RAW_HEADER_DIMENSIONS[1]*0.5, 0.96, RAW_HEADER_DIMENSIONS[1], RAW_HEADER_DIMENSIONS[2])
-        BL("Wooden_Element_13", 0.4784-(RAW_HEADER_DIMENSIONS[2]/2), 2.02, RAW_HEADER_DIMENSIONS[1]*1.5, 0.96, RAW_HEADER_DIMENSIONS[1], RAW_HEADER_DIMENSIONS[2])
+        # BL("Wooden_Element_13", 0.4784-(RAW_HEADER_DIMENSIONS[2]/2), 2.02, RAW_HEADER_DIMENSIONS[1]*0.5, 0.96, RAW_HEADER_DIMENSIONS[1], RAW_HEADER_DIMENSIONS[2])
+        # BL("Wooden_Element_13", 0.4784-(RAW_HEADER_DIMENSIONS[2]/2), 2.02, RAW_HEADER_DIMENSIONS[1]*1.5, 0.96, RAW_HEADER_DIMENSIONS[1], RAW_HEADER_DIMENSIONS[2])
         # BSP("Small_Stud_2", 1.9784, 2.02, 0, 0.88, 0.04, STUD_HEIGHT)
         # LCP("Small_Stud_3", 2.2384, 1.87, 0, 0.48, 0.04, STUD_HEIGHT)
         # LCP("Small_Stud_4", 2.2384, 2.17, 0, 0.48, 0.04, STUD_HEIGHT)
@@ -5240,7 +5264,7 @@ def main():
         # # # IST
         # KING("Wooden_Element_2", 1.2592, 0.02, 0, 2.4384, 0.04, STUD_HEIGHT)
             # L/U
-        # L_U("L_U_Element_1", 1.2592, 0.04+STUD_HEIGHT, STUD_HEIGHT-0.02, 2.4384, 0.04, STUD_HEIGHT)
+        L_U("L_U_Element_1", 1.2592, 0.04+STUD_HEIGHT, STUD_HEIGHT-0.02, 2.4384, 0.04, STUD_HEIGHT)
             # End L/U
         # KING("Wooden_Element_3", 1.2592, 0.4, 0, 2.4384, 0.04, STUD_HEIGHT)
         # KING("Wooden_Element_4", 1.2592, 0.9, 0, 2.4384, 0.04, STUD_HEIGHT)
